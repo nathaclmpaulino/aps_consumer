6f21dfa4be0efa5abdb4c38ef5b21da790c3da22
nathaclmpaulino <nathapaulino17@gmail.com>
2020-11-29 10:53:26 -0300

    [Consumer] Still needs parsing the message, but seems ok to be testing

--- src/config/config.js
+++ src/config/config.js
@@ -11,7 +11,8 @@ module.exports = {
     host: process.env.POSTGRES_HOST || 'localhost',
     username: process.env.POSTGRES_USER || 'localuser',
     password: process.env.POSTGRES_PASSWORD || 'somedummypassword',
-    port: process.env.POSTGRES_PORT || '5432'
+    port: process.env.POSTGRES_PORT || '5432',
+    database: process.env.POSTGRES_DB || 'localdb'
   },
   port: process.env.PORT || 3001,
   frequency: process.env.FREQUENCY || 50000
---
+++ src/utils/psql.js
@@ -0,0 +1,63 @@
+const config    = require('../config/config')
+const { Pool }  = require('pg')
+
+const pool = new Pool({
+  host:     config.psql.host,
+  user:     config.psql.username,
+  password: config.psql.password,
+  port:     config.psql.port,
+  database: config.psql.database
+})
+
+class PSQLInterface {
+  constructor () {
+    this.pool = pool
+    this.client = null
+  }
+
+  async connect() {
+    try {
+      let client = await this.pool.connect()          
+      this.client = client
+    } catch (error) {
+      throw new Error('Unable to connect over PSQL.')
+    }
+  }
+
+  /** 
+   * @param {String} message 
+   */
+
+  parseMessage(message) {    
+    /*
+      Em um array argValues, as seis posições do array representam as 6 colunas da tabela 
+      no banco de dados
+    */
+    console.log(message)
+    //return argValues
+  }
+
+  /**
+   * @param {String} tableName 
+   * @param {Array} argValues 
+   */
+
+  async insertIntoTable(argValues) {
+    try {
+      await this.pool.query('INSERT INTO lights_sensor(time, day, light, temperature, humidity, motion) VALUES($1, $2, $3, $4, $5, $6', [argValues[0], argValues[1], argValues[2], argValues[3], argValues[4], argValues[5]])
+    } catch (error) {
+      throw new Error('Unable to insert data into table')
+    }  
+  }
+
+  release() {
+    try {
+      this.client.release()
+      this.client = null
+    } catch (error) {
+      throw new Error('Unable to release a client for the pool')
+    }   
+  }
+}
+
+module.exports = new PSQLInterface()
